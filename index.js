dc0d6b4c5a5c510c560042fb8b066cdacc697f55
Saurabh Kumar <asksaurabh101@gmail.com>
2021-11-08 13:14:15 +0530

    Initialize repository

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+String.prototype.reverse = function () {
+    return Array.from(this).reverse().join('');
+}
+
+function Phrase(content) {
+
+    this.content = content;
+
+    this.getLowerCase = function (inputString) {
+        return inputString.toLowerCase();
+    }
+
+    this.processedContent = function () {
+        return this.getLowerCase(this.content);
+    }
+
+    this.isPalindrome = function () {
+        return this.processedContent() === this.processedContent().reverse();
+    }
+}
+
+let phrase = new Phrase("Ababac");
+console.log(phrase.isPalindrome());
